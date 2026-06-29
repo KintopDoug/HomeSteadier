5785b29a11476df132a74dbec18b5019de1f1d5e
Doug Kintop <kintopdoug@gmail.com>
2026-06-29 16:53:22 -0400

    adds scaffolded .net core api to aspire project

--- HomeSteadier.AppHost/AppHost.cs
+++ HomeSteadier.AppHost/AppHost.cs
@@ -1,7 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-// Read database name from configuration
+// Read values from configuration
 var databaseName = builder.Configuration["Database:Name"];
+var projectName = builder.Configuration["ProjectName"];
 
 // Add PostgreSQL with init script for conditional database creation
 var postgres = builder.AddPostgres("pgsql")
@@ -9,5 +10,7 @@ var postgres = builder.AddPostgres("pgsql")
     .WithHostPort(5432);
 
 var db = postgres.AddDatabase(databaseName);
+
+builder.AddProject<Projects.Homesteadier_API>(projectName);
 
 builder.Build().Run();
