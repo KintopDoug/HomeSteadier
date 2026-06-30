00e84889c26705fad64d60512fbef921987fa84e
Doug Kintop <kintopdoug@gmail.com>
2026-06-30 09:54:26 -0400

    moves database port definition to appsettings

--- HomeSteadier.AppHost/AppHost.cs
+++ HomeSteadier.AppHost/AppHost.cs
@@ -5,11 +5,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Read values from configuration
 var databaseName = builder.Configuration["Database:Name"];
+var databasePort = builder.Configuration["Database:Port"];
 var projectName = builder.Configuration["ProjectName"];
 
 // Add PostgreSQL with init script for conditional database creation. use pgvector image to enable vector search capabilities.
 var postgres = builder.AddPostgres("pgsql")
-    .WithImage("pgvector/pgvector", "pg17") // or pg16
+    .WithImage("pgvector/pgvector", "pg17")
     .WithDataVolume(databaseName)
     .WithHostPort(5432);
 
@@ -20,7 +21,7 @@ var api = builder.AddProject<Projects.Homesteadier_API>(projectName);
 builder.AddJavaScriptApp("react-frontend", "../ReactApp")
     .WithReference(api)
     .WaitFor(api)
-    // Feeds the API endpoint into your TypeScript process env
+    // Feeds the API endpoint into typeScript process env
     .WithEnvironment("VITE_API_URL", api.GetEndpoint("https"))
     .WithHttpEndpoint(env: "VITE_PORT")
     .WithExternalHttpEndpoints();
