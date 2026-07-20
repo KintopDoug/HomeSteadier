aa57ce477d4271e3681585b2b9277c23534ab3e7
Doug Kintop <kintopdoug@gmail.com>
2026-07-20 12:48:38 -0400

    Revert "adds jwt middleware and authentication to use clerk as jwt provider"
    This reverts commit c0238beef914ca18e7e9a94150f81ca6277860d1.

--- Homesteadier.API/Controllers/UsersController.cs
+++ Homesteadier.API/Controllers/UsersController.cs
@@ -1,25 +1,22 @@
 using Homesteadier.Repository.Repositories;
 using HomeSteadier.Models.Database;
-using HomeSteadier.Models.Security;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Homesteadier.API.Controllers;
 
 [ApiController]
-[Authorize]
 [Route("api/[controller]")]
 public class UsersController : ControllerBase
 {
     private readonly IUserRepository _userRepository;
     private readonly ILogger<UsersController> _logger;
-    private string? ClerkUserId => User.FindFirst(ClaimTypes.Sub.Value())?.Value;
+
     public UsersController(IUserRepository userRepository, ILogger<UsersController> logger)
     {
         _userRepository = userRepository;
         _logger = logger;
     }
-    
+
     [HttpGet]
     public async Task<ActionResult<List<User>>> GetAll()
     {
