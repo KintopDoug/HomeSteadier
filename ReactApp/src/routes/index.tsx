92e95e779ae2fd4ccadc79c34f8f7cbf503078fa
Doug Kintop <kintopdoug@gmail.com>
2026-07-20 11:43:05 -0400

    initial implementation of sign in and sign up components

--- ReactApp/src/routes/index.tsx
+++ ReactApp/src/routes/index.tsx
@@ -1,6 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { SignUp } from '../pages/signUp'
+import { Show, SignInButton, SignUpButton } from '@clerk/react'
+
+const Home = () => (
+  <div className="home-page">
+    <Show when="signed-out">
+      <h1>Welcome to HomeSteadier</h1>
+      <p>Sign in to start managing your homestead.</p>
+      <div className="home-page-actions">
+        <SignInButton mode="modal" />
+        <SignUpButton mode="modal" />
+      </div>
+    </Show>
+    <Show when="signed-in">
+      <h1>Your Homestead</h1>
+      <p>You're signed in. Homestead dashboard coming soon.</p>
+    </Show>
+  </div>
+)
 
 export const Route = createFileRoute('/')({
-  component: SignUp,
+  component: Home,
 })
