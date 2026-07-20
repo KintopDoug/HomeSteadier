46b3731a3d0d8ba02393862e6326509cf43a2bf0
Doug Kintop <kintopdoug@gmail.com>
2026-07-20 12:51:41 -0400

    Revert "initial implementation of sign in and sign up components"
    This reverts commit 4637f5af60c15fe17f9a1f64c098c66e49aecd80.

--- ReactApp/src/components/Layout/Header.tsx
+++ ReactApp/src/components/Layout/Header.tsx
@@ -1,5 +1,3 @@
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
-
 export const Header = () => {
   return (
     <header className="site-header">
@@ -8,15 +6,6 @@ export const Header = () => {
         src="/HomeSteadier-icon-with-text.svg"
         alt="HomeSteadier"
       />
-      <div className="site-header-auth">
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </div>
     </header>
   );
 };
--- ReactApp/src/routes/index.tsx
+++ ReactApp/src/routes/index.tsx
@@ -1,23 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Show, SignInButton, SignUpButton } from '@clerk/react'
-
-const Home = () => (
-  <div className="home-page">
-    <Show when="signed-out">
-      <h1>Welcome to HomeSteadier</h1>
-      <p>Sign in to start managing your homestead.</p>
-      <div className="home-page-actions">
-        <SignInButton mode="modal" />
-        <SignUpButton mode="modal" />
-      </div>
-    </Show>
-    <Show when="signed-in">
-      <h1>Your Homestead</h1>
-      <p>You're signed in. Homestead dashboard coming soon.</p>
-    </Show>
-  </div>
-)
+import { SignUp } from '../pages/signUp'
 
 export const Route = createFileRoute('/')({
-  component: Home,
+  component: SignUp,
 })
